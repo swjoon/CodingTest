ec366deb3680411b7a864a06d2301f192eeefaac
swjoon <113027567+swjoon@users.noreply.github.com>
2024-03-19 11:42:41 +0900

    Add files via upload

---
+++ programmers/Lv1/소수만들기/prime.java
@@ -0,0 +1,42 @@
+package CodingTest.programmers.Lv1.소수만들기;
+
+import java.util.*;
+
+public class prime {
+
+    public static void main(String[] args) {
+        System.out.println(solution(new int[] { 1, 2, 3, 4 }));
+
+    }
+
+    public static int solution(int[] nums) {
+        int answer = 0;
+
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            for (int j = i + 1; j < nums.length - 1; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    list.add(nums[i] + nums[j] + nums[k]);
+                }
+            }
+        }
+
+        for (int num : list) {
+
+            if (prime(num)) {
+                answer += 1;
+            }
+        }
+
+        return answer;
+    }
+
+    public static boolean prime(int num) {
+        for (int i = 2; i <= num / 2; i++) {
+            if (num % i == 0)
+                return false;
+        }
+        return true;
+    }
+}
