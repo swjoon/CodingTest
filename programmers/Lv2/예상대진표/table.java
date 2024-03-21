940dc7e6c2909e99f72629445b7e31daef4f51f6
swjoon <jetkid1446@gmail.com>
2024-03-21 16:11:02 +0900

    commit

---
+++ programmers/Lv2/예상대진표/table.java
@@ -0,0 +1,38 @@
+package CodingTest.programmers.Lv2.예상대진표;
+
+public class table {
+    public static int solution(int n, int a, int b) {
+        System.out.printf("%d, %d, %d \n", n, a, b);
+
+        int answer = 1;
+
+        if (a > b) {
+            int x = a;
+            a = b;
+            b = x;
+        }
+
+        while (!(b - a == 1 && (a / 2 != b / 2))) {
+
+            a = cal(a);
+            b = cal(b);
+
+            answer = answer + 1;
+        }
+
+        System.out.printf("%d \n", answer);
+
+        return answer;
+    }
+
+    public static int cal(int num) {
+
+        int x = num % 2;
+
+        if (x == 0) {
+            return num = num / 2;
+        } else {
+            return num = num / 2 + 1;
+        }
+    }
+}
---
+++ programmers/Lv2/점찍기/dot.java
@@ -0,0 +1,19 @@
+package CodingTest.programmers.Lv2.점찍기;
+
+public class dot {
+
+    public static long solution(int k, int d) {
+        long answer = 0;
+        long dd = (long) Math.pow(d, 2);
+
+        for (int y = 0; y <= d; y += k) {
+            // d^2 - y^2 보다 작은 k의 배수
+            long yy = (long) Math.pow(y, 2);
+            int num = (int) Math.sqrt(dd - yy) / k + 1;
+            answer += num;
+
+        }
+
+        return answer;
+    }
+}
