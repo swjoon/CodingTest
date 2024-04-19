aa8b3e3ca98fb2fb01c3877deade09c456f08543
swjoon <jetkid1446@gmail.com>
2024-04-19 15:51:27 +0900

    commit

---
+++ programmers/Lv2/과제진행하기/assignment.java
@@ -0,0 +1,12 @@
+package 과제진행하기;
+
+public class assignment {
+    public static void main(String[] args){
+       
+    }
+
+    public static String[] solution(String[][] plans) {
+        String[] answer = {};
+        return answer;
+    }
+}
--- programmers/Lv3/인사고과/review.java
+++ programmers/Lv3/인사고과/review.java
@@ -8,37 +8,29 @@ public class review {
         System.out.println(solution(new int[][]{{2,2},{3,4},{10,2},{3,2},{2,5},{7,1},{1,1}}));
     }
 
+
+    // 정확성 88/100 -> 시간 초과 3 -> 2중for문 비효율적이라 생각.
     public static int solution(int[][] scores) {
         List<int[]> scoreslist = new ArrayList<>();
         List<Integer> scoresnum = new ArrayList<>();
         int answer = 0;
-        int max = 0;
-
-        for(int i = 0; i < scores.length; i++){
-            if(scores[i][0]> scores[i][1]){ // 작은 수를 [0] 에 넣기.
-                int num = scores[i][0];
-                scores[i][0] = scores[i][1];
-                scores[i][1] = num;
-            }
-
-            if(scores[i][0] > max){ // 작은값 중 max 찾기.
-                max = scores[i][0];
-            }
-
-            scoreslist.add(scores[i]); // list 에 추가
-        }
 
         int[] mainman = new int[]{scores[0][0],scores[0][1]};
         int mainmanscore = scores[0][0] + scores[0][1];
-        System.out.println("완호의 점수: " + Arrays.toString(mainman) + " 총점: " + mainmanscore ); //
-
-        System.out.println("현재 배열: " + Arrays.deepToString(scoreslist.toArray())); //
-
-        int comparemax = max;
+        System.out.println("현재 배열: " + Arrays.deepToString(scores)); //
         
-        System.out.println("max: "+ comparemax ); //
+        for(int i = 0; i < scores.length; i++){
+            scoreslist.add(scores[i]); // list 에 추가
+            for(int j = 0; j < scores.length; j++){
+                if(scores[0][0]<scores[j][0] && scores[0][1] < scores[j][1]) return -1;
+                if(scores[i][0]<scores[j][0] && scores[i][1] < scores[j][1]) {
+                    scoreslist.remove(scores[i]);
+                    break;
+                }                
+            }
+        }
 
-        scoreslist.removeIf(o -> o[1] < comparemax);
+        System.out.println("완호의 점수: " + Arrays.toString(mainman) + " 총점: " + mainmanscore ); //
 
         System.out.println("삭제 후 배열: " + Arrays.deepToString(scoreslist.toArray())); //
 
@@ -65,3 +57,12 @@ public class review {
         return answer;
     }
 }
+// test1
+// 1. scores에서 인사고과, 동료평가중 큰값과 작은 값 구분. 후 작은값 max 추출, 큰값 추출  -> 문제 잘못이해
+// 2. 작은값 max 보다 작은 큰값 배열에서 제외
+
+// test2
+// 이중 for문 비효율적.
+
+// test3
+// 
