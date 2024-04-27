0aef4293c1d27df5ba38d68f36ef7b6dc303d9ff
swjoon <jetkid1446@gmail.com>
2024-04-27 23:58:03 +0900

    commit

--- programmers/Lv3/최고의 집합/set.java
+++ programmers/Lv3/최고의 집합/set.java
@@ -3,24 +3,17 @@ import java.util.*;
 public class set {
     
     public static int[] solution(int n, int s) {
-        int[] answer = {};
-        
-        
-        int num = (int) (s/n);
-        
+        int[] answer = {};   
+        int num = (int) (s/n);    
         int remain = (int) s%n;
-        
         if(s<n){
             answer = new int[] {-1};
         }else{
-            answer = new int[n];
-            
-            Arrays.fill(answer, num);
-        
+            answer = new int[n];      
+            Arrays.fill(answer, num);   
             for (int i = 0; i < remain; i++){
                 answer[i]++;
-            }
-        
+            }   
             Arrays.sort(answer);
         }
         
