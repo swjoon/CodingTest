c1d4c8d79307ea27650599f85ebc82ec07d47999
swjoon <jetkid1446@gmail.com>
2024-05-08 08:54:37 +0900

    commit

---
+++ programmers/Lv3/네트워크/Network.java
@@ -0,0 +1,18 @@
+package 네트워크;
+
+public class Network {
+    public static void main(String[] args){
+
+    }
+
+    // (DFS/BFS)
+    public static int solution(int n, int[][] computers) {
+        int answer = 0;
+
+        
+
+
+
+        return answer;
+    }
+}
--- programmers/Lv3/이중우선순위큐/Queuee.java
+++ programmers/Lv3/이중우선순위큐/Queuee.java
@@ -7,7 +7,7 @@ public class Queuee {
         // solution(new String[]{"I 16", "I -5643", "D -1", "D 1", "D 1", "I 123", "D -1"});
         solution(new String[]{"I -45", "I 653", "D 1", "I -642", "I 45", "I 97", "D 1", "D -1", "I 333"});
     }
-
+    // priority queue
     public static int[] solution(String[] operations) {
         ArrayList<Integer> list = new ArrayList<>();
         String[] splitString = new String[2];
