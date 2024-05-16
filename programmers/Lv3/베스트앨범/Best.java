bacbce6f037ac98bd543290a9dcc617697eb2df4
swjoon <jetkid1446@gmail.com>
2024-05-16 17:18:19 +0900

    commit

--- programmers/Lv3/베스트앨범/Best.java
+++ programmers/Lv3/베스트앨범/Best.java
@@ -9,11 +9,10 @@ import java.util.List;
 public class Best {
     public static void main(String[] args) {
         System.out.println(Arrays.toString(solution(new String[] { "classic", "pop", "classic", "classic", "pop" },
-                new int[] { 500, 600, 500, 800, 2500 })));
+                new int[] { 500, 600, 150, 800, 2500 })));
 
     }
 
-    // 1차 test 오류
     public static int[] solution(String[] genres, int[] plays) {
         int[] answer = {};
         HashMap<String, ArrayList<int[]>> map = new HashMap<>();
@@ -22,10 +21,12 @@ public class Best {
         for (int i = 0; i < genres.length; i++) {
             if (!map.containsKey(genres[i])) {
                 map.put(genres[i], new ArrayList<>());
+                map.get(genres[i]).add(new int[] { i, plays[i] });
                 maxValue.put(genres[i], plays[i]);
+            }else{
+                map.get(genres[i]).add(new int[] { i, plays[i] });
+                maxValue.put(genres[i], maxValue.get(genres[i]) + plays[i]);
             }
-            map.get(genres[i]).add(new int[] { i, plays[i] });
-            maxValue.put(genres[i], maxValue.get(genres[i]) + plays[i]);
         }
 
         List<String> lists = new ArrayList<>(maxValue.keySet());
@@ -35,7 +36,7 @@ public class Best {
         for (String list : lists) {
             List<int[]> genre = map.get(list);
             Collections.sort(genre, (o1, o2) -> o2[1] - o1[1]);
-            if (genre.size() > 2 && genre.get(1) == genre.get(2)) {
+            if (genre.size() > 2 && genre.get(1)[1] == genre.get(2)[1]) {
                 genre.get(1)[0] = (genre.get(1)[0] < genre.get(2)[0]) ? genre.get(1)[0] : genre.get(2)[0];
             }
             for (int i = 0; i < Math.min(2, genre.size()); i++) {
@@ -46,6 +47,7 @@ public class Best {
         for (int i = 0; i < answer.length; i++) {
             answer[i] = answers.get(i);
         }
+
         return answer;
     }
 
