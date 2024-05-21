618fd2cde49bc905d5d915128f1694476c05a4fe
swjoon <jetkid1446@gmail.com>
2024-05-21 23:54:45 +0900

    commit

---
+++ programmers/Lv3/가장먼노드/Node.java
@@ -0,0 +1,18 @@
+package 가장먼노드;
+
+public class Node {
+    public static void main(String[] args){
+        solution(6, new int[][]{{3,6},{4,3},{3,2},{1,3},{1,2},{2,4},{5,2}});
+    }
+
+    public static int solution(int n, int[][] edge) {
+        int answer = 0;
+        
+
+
+
+
+
+        return answer;
+    }
+}
