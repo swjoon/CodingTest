f987ec3ff110a154add2eb6522c7c4f63c952621
swjoon <jetkid1446@gmail.com>
2024-05-23 14:27:07 +0900

    commit

---
+++ programmers/Lv3/보석쇼핑/gem.java
@@ -0,0 +1,19 @@
+package 보석쇼핑;
+
+import java.util.*;
+
+public class gem {
+    public static void main(String[] args) {
+        solution(new String[] { "DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA" });
+        solution(new String[] { "AA", "AB", "AC", "AA", "AC" });
+    }
+
+    public static int[] solution(String[] gems) {
+        int[] answer = {};
+      
+
+
+
+        return answer;
+    }
+}
