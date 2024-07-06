0463551f9ac36ad0ef08ee1b4693ffc46f945f63
swjoon <jetkid1446@gmail.com>
2024-07-06 15:43:21 +0900

    7/6

---
+++ programmers/Lv3/순위/Rank.java
@@ -0,0 +1,21 @@
+package 순위;
+
+import java.util.*;
+
+public class Rank {
+    public static void main(String[] args){
+        solution(5, new int[][]{{4,3},{4,2},{3,2},{1,2},{2,5}});
+    }
+
+    public static int solution(int n, int[][] results) {
+        int answer = 0;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int[] result: results){
+            
+        }
+
+
+
+        return answer;
+    }
+}
