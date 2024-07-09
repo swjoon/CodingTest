6248c4c8a47a89ce056fa4d9477227e68ebe15f5
swjoon <jetkid1446@gmail.com>
2024-07-09 13:13:19 +0900

    7/9

---
+++ programmers/Lv3/셔틀버스/Bus.java
@@ -0,0 +1,17 @@
+package 셔틀버스;
+
+public class Bus {
+    public static void main(String[] args){
+        solution(1, 1, 5, new String[]{"08:00", "08:01", "08:02", "08:03"});
+    }
+
+    public static String solution(int n, int t, int m, String[] timetable) {
+        String answer = "";
+
+
+
+
+        
+        return answer;
+    }
+}
