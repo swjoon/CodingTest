da1683f9d89be74282dc8b4240bdfcf71c1fcbb7
swjoon <jetkid1446@gmail.com>
2024-07-10 23:41:56 +0900

    commit

---
+++ programmers/Lv3/자물쇠와열쇠/Lock.java
@@ -0,0 +1,15 @@
+package 자물쇠와열쇠;
+
+public class Lock {
+    public static void main(String[] args){
+
+    }
+
+    public static boolean solution(int[][] key, int[][] lock) {
+        boolean answer = true;
+
+
+
+        return answer;
+    }
+}
