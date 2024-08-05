6217094eba41c0f0ae02e6282af85480609d721d
swjoon <jetkid1446@gmail.com>
2024-08-05 14:47:43 +0900

    8/5

--- baekjoon/gold/보석도둑/Jewel.java
+++ baekjoon/gold/보석도둑/Jewel.java
@@ -14,46 +14,57 @@ public class Jewel {
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
 
-        HashMap<Integer, PriorityQueue<Integer>> map = new HashMap<>();
+        jewelItem[] jewel = new jewelItem[N];
+
         PriorityQueue<Integer> bag = new PriorityQueue<>();
+        PriorityQueue<Integer> list = new PriorityQueue<>(Collections.reverseOrder());
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            int m = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
             int value = Integer.parseInt(st.nextToken());
 
-            map.putIfAbsent(m, new PriorityQueue<>((o1, o2) -> o2 - o1));
-            map.get(m).add(value);
+            jewel[i] = new jewelItem(w, value);
         }
+
+        Arrays.sort(jewel, new Comparator<jewelItem>() {
+            public int compare(jewelItem o1, jewelItem o2) {
+                if (o1.weight == o2.weight) {
+                    return o2.value - o1.value;
+                }
+                return o1.weight - o2.weight;
+            }
+        });
 
         for (int i = 0; i < K; i++) {
             bag.add(Integer.parseInt(br.readLine()));
         }
 
-        int maxsum = 0;
-
+        long maxsum = 0;
+        int jnum = 0;
         while (!bag.isEmpty()) {
             int now = bag.poll();
-            int maxV = 0;
-            int W = 0;
 
-            for (int i = 1; i <= now; i++) {
-                PriorityQueue<Integer> pq = map.get(i);
-                if (pq != null && !pq.isEmpty()) {
-                    int value = map.get(i).peek();
-                    if (value >= maxV) {
-                        W = i;
-                        maxV = value;
-                    }
-                }
+            while (jnum < N && jewel[jnum].weight <= now) {
+                list.add(jewel[jnum++].value);
             }
 
-            if (map.get(W) != null) {
-                map.get(W).poll();
-                maxsum += maxV;
+            if (!list.isEmpty()) {
+                maxsum += list.poll();
             }
+
         }
 
         System.out.println(maxsum);
+    }
+}
+
+class jewelItem {
+    int weight;
+    int value;
+
+    jewelItem(int weight, int value) {
+        this.weight = weight;
+        this.value = value;
     }
 }
