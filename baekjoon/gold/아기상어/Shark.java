790fbceb51c9f2a43179842d9c217c260c5b20b2
swjoon <jetkid1446@gmail.com>
2024-08-22 12:57:06 +0900

    8/22 bfs

--- baekjoon/gold/아기상어/Shark.java
+++ baekjoon/gold/아기상어/Shark.java
@@ -6,13 +6,14 @@ import java.io.*;
 public class Shark {
     static int N;
     static int fish;
-    static int[] start;
+    static babyshark now;
     static int[][] map;
+    static int[] X = { 0, 0, -1, 1 };
+    static int[] Y = { 1, -1, 0, 0 };
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         fish = 0;
-        start = new int[2];
 
         N = Integer.parseInt(br.readLine());
 
@@ -23,35 +24,88 @@ public class Shark {
             for (int x = 0; x < N; x++) {
                 map[y][x] = Integer.parseInt(st.nextToken());
                 if (map[y][x] == 9) {
-                    start[0] = x;
-                    start[1] = y;
-                } else if (map[y][x] != 0) {
-                    fish++;
+                    map[y][x] = 0;
+                    now = new babyshark(x, y, 0, 2);
                 }
             }
         }
 
-        if (fish == 0) {
-            System.out.println(0);
-            return;
+        int eat = 0;
+
+        while (BFS(now)) {
+            if (++eat == now.size) {
+                eat = 0;
+                now.size++;
+            }
         }
 
-        
+        System.out.println(now.cnt);
+
     }
 
-    static void BFS() {
-        Queue<Point> q = new LinkedList<>();
+    static boolean BFS(babyshark shark) {
+        boolean[][] visited = new boolean[N][N];
+        Queue<babyshark> q = new LinkedList<>();
+
+        PriorityQueue<babyshark> pq = new PriorityQueue<>((o1, o2) -> {
+            if (o1.cnt != o2.cnt) {
+                return o1.cnt - o2.cnt;
+            }
+            if (o1.y != o2.y) {
+                return o1.y - o2.y;
+            }
+            return o1.x - o2.x;
+        });
+
+        visited[shark.y][shark.x] = true;
+        q.add(shark);
+
+        while (!q.isEmpty()) {
+            babyshark s = q.poll();
+
+            if (!pq.isEmpty() && pq.peek().cnt < s.cnt) {
+                break;
+            }
+
+            for (int i = 0; i < 4; i++) {
+                int nextx = s.x + X[i];
+                int nexty = s.y + Y[i];
+                int nextcnt = s.cnt + 1;
+                if (nextx < 0 || nexty < 0 || nextx >= N || nexty >= N || visited[nexty][nextx]
+                        || map[nexty][nextx] > s.size) {
+                    continue;
+                }
+
+                visited[nexty][nextx] = true;
+
+                if (map[nexty][nextx] > 0 && map[nexty][nextx] < shark.size) {
+                    pq.add(new babyshark(nextx, nexty, nextcnt, s.size));
+                } else {
+                    q.add(new babyshark(nextx, nexty, nextcnt, s.size));
+                }
+            }
+        }
+
+        if (!pq.isEmpty()) {
+            babyshark target = pq.poll();
+            now = new babyshark(target.x, target.y, target.cnt, target.size);
+            map[now.y][now.x] = 0;
+            return true;
+        }
+
+        return false;
     }
 }
 
-class Point {
-
+class babyshark {
     int x, y;
     int cnt;
+    int size;
 
-    Point(int x, int y, int cnt) {
+    babyshark(int x, int y, int cnt, int size) {
         this.x = x;
         this.y = y;
         this.cnt = cnt;
+        this.size = size;
     }
 }
