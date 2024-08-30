8df68ff47f2092c28659115f8fe616d241d2d7a1
swjoon <jetkid1446@gmail.com>
2024-08-30 12:55:23 +0900

    8/30 BFS

--- baekjoon/gold/연구소3/Laboratory.java
+++ baekjoon/gold/연구소3/Laboratory.java
@@ -5,10 +5,23 @@ import java.io.*;
 
 public class Laboratory {
     static int N, M;
+    static int uninfect = 0;
+    static int ans = Integer.MAX_VALUE;
     static char[][] map;
-    static boolean[][] visited;
     static int[] dirX = { 0, 0, -1, 1 };
     static int[] dirY = { -1, 1, 0, 0 };
+    static List<int[]> list = new ArrayList<>();
+
+    static class Point {
+        int x, y;
+        int cnt;
+
+        Point(int x, int y, int cnt) {
+            this.x = x;
+            this.y = y;
+            this.cnt = cnt;
+        }
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -18,7 +31,6 @@ public class Laboratory {
         M = Integer.parseInt(st.nextToken());
 
         map = new char[N][N];
-        visited = new boolean[N][N];
 
         for (int y = 0; y < N; y++) {
             st = new StringTokenizer(br.readLine());
@@ -27,34 +39,93 @@ public class Laboratory {
                 switch (num) {
                     case 0:
                         map[y][x] = '.';
+                        ++uninfect;
                         break;
                     case 1:
                         map[y][x] = '-';
                         break;
                     default:
+                        list.add(new int[] { x, y });
                         map[y][x] = '*';
                         break;
                 }
             }
         }
 
-        for (int i = 0; i < N; i++) {
-            System.out.println(Arrays.toString(map[i]));
+        if (uninfect == 0) {
+            System.out.println(0);
+            return;
         }
+
+        List<List<int[]>> trylist = comb();
+
+        for (List<int[]> tryL : trylist) {
+            BFS(tryL);
+        }
+
+        if (ans == Integer.MAX_VALUE) {
+            System.out.println(-1);
+            return;
+        }
+
+        System.out.println(ans);
     }
 
-    static void BFS() {
+    static void BFS(List<int[]> tryL) {
+        boolean[][] visited = new boolean[N][N];
+        Queue<Point> q = new LinkedList<>();
 
-    }
+        int infect = 0;
 
-    static boolean check() {
-        for (int y = 0; y < N; y++) {
-            for (int x = 0; x < N; x++) {
-                if (map[y][x] == 0) {
-                    return false;
+        for (int[] i : tryL) {
+            q.add(new Point(i[0], i[1], 0));
+            visited[i[1]][i[0]] = true;
+        }
+
+        while (!q.isEmpty()) {
+            Point p = q.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int nextx = p.x + dirX[i];
+                int nexty = p.y + dirY[i];
+
+                if (nextx < 0 || nexty < 0 || nextx >= N || nexty >= N || map[nexty][nextx] == '-'
+                        || visited[nexty][nextx]) {
+                    continue;
+                }
+
+                visited[nexty][nextx] = true;
+
+                q.add(new Point(nextx, nexty, p.cnt + 1));
+
+                if (map[nexty][nextx] == '.') {
+                    ++infect;
+                }
+
+                if (uninfect == infect) {
+                    ans = Math.min(ans, p.cnt + 1);
+                    return;
                 }
             }
         }
-        return true;
+    }
+
+    static void combination(int start, int depth, List<List<int[]>> L, List<int[]> currentCombination) {
+        if (depth == M) {
+            L.add(new ArrayList<>(currentCombination));
+            return;
+        }
+
+        for (int i = start; i < list.size(); i++) {
+            currentCombination.add(list.get(i));
+            combination(i + 1, depth + 1, L, currentCombination);
+            currentCombination.remove(currentCombination.size() - 1);
+        }
+    }
+
+    static List<List<int[]>> comb() {
+        List<List<int[]>> L = new ArrayList<>();
+        combination(0, 0, L, new ArrayList<>());
+        return L;
     }
 }
