6953edd10e989c354c27018779504420c1dc0db0
swjoon <jetkid1446@gmail.com>
2024-10-22 21:22:00 +0900

    10/22 union-find

---
+++ programmers/Lv4/호텔방배정/Hotel.java
@@ -0,0 +1,46 @@
+package programmers.Lv4.호텔방배정;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Hotel {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(solution(10, new long[] { 1, 3, 4, 1, 3, 1, 2, 7, })));
+    }
+
+    public static long[] solution(long k, long[] room_number) {
+        Map<Long, Long> map = new HashMap<>();
+        int N = room_number.length;
+        long[] ans = new long[N];
+
+        for (int i = 0; i < N; i++) {
+            long select = room_number[i];
+            ans[i] = find(map, select);
+        }
+
+        return ans;
+    }
+
+    public static long find(Map<Long, Long> map, long select) {
+        long parent = findParent(map, select);
+
+        map.put(select, parent + 1);
+
+        return parent;
+    }
+
+    public static long findParent(Map<Long, Long> map, long select) {
+        // 빈방 선택
+        if (!map.containsKey(select)) {
+            map.put(select, select+1);
+            return select;
+        }
+
+        long parent = findParent(map, map.get(select));
+
+        map.put(select, parent);
+
+        return parent;
+    }
+}
