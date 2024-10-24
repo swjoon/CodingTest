bbabef0e99cd3bb0d0069142e8f5a6b2b45fccf0
swjoon <jetkid1446@gmail.com>
2024-10-24 23:21:04 +0900

    10/24 dp

---
+++ programmers/Lv3/N으로표현/N.java
@@ -0,0 +1,16 @@
+package N으로표현;
+
+public class N {
+    public static void main(String[] args){
+        System.out.println(solution(5, 12));
+    }
+
+    public static int solution(int N, int number) {
+        int answer = 0;
+
+
+
+        
+        return answer;
+    }
+}
