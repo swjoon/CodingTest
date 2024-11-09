d34f41d1c817881866db7d830eaaef53bfcc4b85
swjoon <jetkid1446@gmail.com>
2024-11-09 11:05:23 +0900

    11/08 dfs

--- baekjoon/gold/사다리조작/Ladder.java
+++ baekjoon/gold/사다리조작/Ladder.java
@@ -4,10 +4,21 @@ import java.util.*;
 import java.io.*;
 
 public class Ladder {
+    static int N,M,H;
     public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        H = Integer.parseInt(st.nextToken());
+
+        for(int i = 0; i < M; i++){
+
+        }
+    }
+
+    static void DFS(){
         
     }
 }
