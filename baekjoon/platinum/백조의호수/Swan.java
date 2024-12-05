707b5de660606579008e8ca3a07be8c284f5278c
swjoon <jetkid1446@gmail.com>
2024-12-05 10:02:42 +0900

    12/05 bfs

--- baekjoon/platinum/백조의호수/Swan.java
+++ baekjoon/platinum/백조의호수/Swan.java
@@ -1,118 +1,165 @@
 package baekjoon.platinum.백조의호수;
 
+import java.io.*;
 import java.util.*;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+
+// 문제 풀이 순서
+// ICE 지역 각 칸의 두께 측정
+// 오리간 ICE를 거치는 경로 중 ICE의 두께가 최소인 경로 찾기
 
 public class Swan {
-    static int R, C;
+    static int X, Y;
     static char[][] map;
+    static int[][] iceMap;
+    static boolean[][] mapVisited;
     static int[] dirX = { 0, 0, 1, -1 };
     static int[] dirY = { 1, -1, 0, 0 };
-    static boolean[][] watervisited;
-    static Queue<Point> water = new LinkedList<>();
-    static Queue<Point> meltice = new LinkedList<>();
+
+    static class Point implements Comparable<Point> {
+        int x, y, iceCnt;
+
+        public Point(int x, int y, int iceCnt) {
+            this.x = x;
+            this.y = y;
+            this.iceCnt = iceCnt;
+        }
+
+        @Override
+        public int compareTo(Point o) {
+            return Integer.compare(this.iceCnt, o.iceCnt);
+        }
+
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        R = Integer.parseInt(st.nextToken());
-        C = Integer.parseInt(st.nextToken());
+        Y = Integer.parseInt(st.nextToken());
+        X = Integer.parseInt(st.nextToken());
 
-        map = new char[R][C];
-        watervisited = new boolean[R][C];
+        map = new char[Y][X];
+        iceMap = new int[Y][X];
+        mapVisited = new boolean[Y][X];
 
-        Point start = null;
+        for (int y = 0; y < Y; y++) {
+            map[y] = br.readLine().toCharArray();
+        }
 
-        for (int y = 0; y < R; y++) {
-            String line = br.readLine();
-            map[y] = line.toCharArray();
-            for (int x = 0; x < C; x++) {
-                if (map[y][x] == 'L' && start == null) {
-                    start = new Point(x, y);
+        Queue<Point> ice = new LinkedList<>();
+        Point startPoint = new Point(0, 0, 0);
+
+        for (int y = 0; y < Y; y++) {
+            for (int x = 0; x < X; x++) {
+                if (map[y][x] == 'L') {
+                    startPoint.x = x;
+                    startPoint.y = y;
                 }
-                if (map[y][x] == 'L' || map[y][x] == '.') {
-                    water.add(new Point(x, y));
-                    watervisited[y][x] = true;
+                if ((map[y][x] == '.' || map[y][x] == 'L') && !mapVisited[y][x]) {
+                    mapVisited[y][x] = true;
+                    ice.addAll(findIce(new Point(x, y, 0)));
                 }
             }
         }
 
-        int day = 0;
+        makeIceMap(ice);
 
-        while (!meet(start)) {
-            melt();
-            day++;
-        }
-
-        System.out.println(day);
-
+        System.out.println(findRoute(startPoint));
     }
 
-    static boolean meet(Point start) {
-        Queue<Point> q = new LinkedList<>();
-        boolean[][] visited = new boolean[R][C];
-        visited[start.y][start.x] = true;
+    // 얼음 경계선 찾기
+    public static Queue<Point> findIce(Point input) {
+        Queue<Point> water = new LinkedList<>();
+        Queue<Point> ice = new LinkedList<>();
+        water.add(input);
 
-        q.add(start);
-        while (!q.isEmpty()) {
-            Point p = q.poll();
-
-            for (int i = 0; i < 4; i++) {
-                int nextx = p.x + dirX[i];
-                int nexty = p.y + dirY[i];
-
-                if (nextx < 0 || nexty < 0 || nextx >= C || nexty >= R || visited[nexty][nextx]
-                        || map[nexty][nextx] == 'X') {
-                    continue;
-                }
-
-                if (map[nexty][nextx] == 'L') {
-                    return true;
-                }
-
-                visited[nexty][nextx] = true;
-
-                q.add(new Point(nextx, nexty));
-            }
-        }
-        return false;
-    }
-
-    static void melt() {
         while (!water.isEmpty()) {
-            Point p = water.poll();
+            Point now = water.poll();
 
             for (int i = 0; i < 4; i++) {
-                int nextx = p.x + dirX[i];
-                int nexty = p.y + dirY[i];
+                int nextX = now.x + dirX[i];
+                int nextY = now.y + dirY[i];
 
-                if (nextx < 0 || nexty < 0 || nextx >= C || nexty >= R || watervisited[nexty][nextx]) {
+                if (nextX >= 0 && nextY >= 0 && nextX < X && nextY < Y && !mapVisited[nextY][nextX]) {
+                    mapVisited[nextY][nextX] = true;
+                    if (map[nextY][nextX] == 'X') {
+                        iceMap[nextY][nextX] = 1;
+                        ice.add(new Point(nextX, nextY, 1));
+                        continue;
+                    }
+                    water.add(new Point(nextX, nextY, 0));
+                }
+            }
+        }
+        return ice;
+    }
+
+    // 얼음섬 두께 적용 맵 생성
+    public static void makeIceMap(Queue<Point> ice) {
+        while (!ice.isEmpty()) {
+            Point now = ice.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int nextX = now.x + dirX[i];
+                int nextY = now.y + dirY[i];
+                int nextCnt = now.iceCnt + 1;
+
+                if (nextX >= 0 && nextY >= 0 && nextX < X && nextY < Y && !mapVisited[nextY][nextX]
+                        && map[nextY][nextX] == 'X') {
+                    mapVisited[nextY][nextX] = true;
+                    iceMap[nextY][nextX] = nextCnt;
+                    ice.add(new Point(nextX, nextY, nextCnt));
+                }
+            }
+        }
+    }
+
+    // 길찾기
+    public static int findRoute(Point startPoint) {
+        PriorityQueue<Point> point = new PriorityQueue<>();
+        boolean[][] visited = new boolean[Y][X];
+        int[][] route = new int[Y][X];
+        int day = Integer.MAX_VALUE;
+
+        for (int i = 0; i < Y; i++) {
+            Arrays.fill(route[i], Integer.MAX_VALUE);
+        }
+
+        route[startPoint.y][startPoint.x] = 0;
+
+        point.add(startPoint);
+
+        while (!point.isEmpty()) {
+            Point now = point.poll();
+
+            if (visited[now.y][now.x]) {
+                continue;
+            }
+
+            visited[now.y][now.x] = true;
+
+            if (map[now.y][now.x] == 'L' && (now.x != startPoint.x || now.y != startPoint.y)) {
+                day = now.iceCnt;
+                break;
+            }
+
+            for (int i = 0; i < 4; i++) {
+                int nextX = now.x + dirX[i];
+                int nextY = now.y + dirY[i];
+
+                if (nextX < 0 || nextY < 0 || nextX >= X || nextY >= Y) {
                     continue;
                 }
 
-                if (map[nexty][nextx] == 'X') {
-                    map[nexty][nextx] = '.';
-                    meltice.add(new Point(nextx, nexty));
+                int nextIceCnt = Math.max(now.iceCnt, iceMap[nextY][nextX]);
+
+                if (nextIceCnt < route[nextY][nextX]) {
+                    route[nextY][nextX] = nextIceCnt;
+                    point.add(new Point(nextX, nextY, nextIceCnt));
                 }
-
-                watervisited[nexty][nextx] = true;
             }
-            
+
         }
-        Queue<Point> temp = water;
-        water = meltice;
-        meltice = temp;
-    }
-}
-
-class Point {
-    int x, y;
-
-    Point(int x, int y) {
-        this.x = x;
-        this.y = y;
+        return day;
     }
 }
