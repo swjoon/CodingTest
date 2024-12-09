10c1def93b371845735670cb0ec535e51caa1425
swjoon <jetkid1446@gmail.com>
2024-12-09 22:09:32 +0900

    12/09 메모리 참고

--- baekjoon/gold/문자열폭발/Explosion.java
+++ baekjoon/gold/문자열폭발/Explosion.java
@@ -1,70 +1,37 @@
 package baekjoon.gold.문자열폭발;
 
-import java.util.*;
 import java.io.*;
 
 public class Explosion {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        char[] input = br.readLine().toCharArray();
-        Deque<Character> charList = new ArrayDeque<>();
 
-        for (char c : input) {
-            charList.add(c);
-        }
-
+        String input = br.readLine();
         String trigger = br.readLine();
-        char[] triggerList = trigger.toCharArray();
 
         StringBuilder sb = new StringBuilder();
 
-        while (!charList.isEmpty()) {
-            char character = charList.peek();
-            if (character == triggerList[0]) {
-                sb.append(triggerCheck(trigger, triggerList, charList));
-            } else {
-                sb.append(charList.poll());
+        for (int i = 0; i < input.length(); i++) {
+            sb.append(input.charAt(i));
+
+            if (sb.length() >= trigger.length() && checkTrigger(sb, trigger)) {
+                explosion(sb, trigger);
             }
         }
 
         System.out.println(sb.length() == 0 ? "FRULA" : sb.toString());
     }
 
-    static String triggerCheck(String trigger, char[] triggerList, Deque<Character> charList) {
-        StringBuilder sb = new StringBuilder();
-        sb.append(charList.poll());
-
-        int now = 1;
-
-        while (!charList.isEmpty()) {
-            char character = charList.peek();
-
-            if (now >= trigger.length()) {
-                return "";
+    private static boolean checkTrigger(StringBuilder sb, String trigger) {
+        for (int i = 0; i < trigger.length(); i++) {
+            if (sb.charAt(sb.length() - trigger.length() + i) != trigger.charAt(i)) {
+                return false;
             }
-
-            if (character == triggerList[0]) {
-                String check = triggerCheck(trigger, triggerList, charList);
-                if (check.length() != 0) {
-                    sb.append(check);
-                    return sb.toString();
-                }
-                continue;
-            }
-
-            if (character == triggerList[now++]) {
-                sb.append(charList.poll());
-                continue;
-            }
-
-            sb.append(charList.poll());
-            break;
         }
+        return true;
+    }
 
-        if (trigger.equals(sb.toString())) {
-            return "";
-        }
-
-        return sb.toString();
+    private static void explosion(StringBuilder sb, String trigger) {
+        sb.delete(sb.length()-trigger.length(), sb.length());
     }
 }
