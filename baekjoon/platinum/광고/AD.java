d9ba8352b49acccf0627445ffa408bcc8652f0db
swjoon <jetkid1446@gmail.com>
2024-12-23 15:19:20 +0900

    12/23 KMP 알고리즘 학습

--- baekjoon/platinum/광고/AD.java
+++ baekjoon/platinum/광고/AD.java
@@ -1,90 +1,42 @@
 package baekjoon.platinum.광고;
 
-import java.util.*;
 import java.io.*;
 
 public class AD {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int size = Integer.parseInt(br.readLine());
-        String[] input = br.readLine().split("");
-        Queue<String> queue = new LinkedList<>(Arrays.asList(input));
-        StringBuilder ad = new StringBuilder();
+        int L = Integer.parseInt(br.readLine());
+        String S = br.readLine();
 
-        int dup = 1;
+        int[] pi = kmp(S);
 
-        for (int i = 0; i < size; i++) {
-            if (!input[i].equals(input[i + 1])) {
-                break;
-            }
-            dup++;
-        }
+        int minAd = L - pi[L - 1];
 
-        String start = queue.poll();
-        ad.append(start);
-
-        int count = 0;
-
-        while (!queue.isEmpty()) {
-            // 현재까지 인정된 광고문자열 시작과 동일하다면 중복되는지 체크
-            if (queue.peek().equals(ad.substring(0, 1))) {
-                String str = check(queue, ad, dup);
-
-                // 1. 문자열이 완벽히 일치 2. 문자열이 일치하지만 광고판 사이즈가 초과되어서 중간에 끊기는 경우
-                if (str.length() == ad.length()) {
-                    count++;
-                } else {
-                    String add = ad.toString();
-                    for (int i = 0; i < count; i++) {
-                        ad.append(add);
-                    }
-                    ad.append(str);
-                    count = 0;
-                }
-                continue;
-            }
-
-            String st = queue.poll();
-            String add = ad.toString();
-
-            for (int i = 0; i < count; i++) {
-                ad.append(add);
-            }
-
-            ad.append(st);
-        }
-
-        System.out.println(ad.length());
+        System.out.println(minAd);
     }
 
-    private static String check(Queue<String> queue, StringBuilder ad, int dup) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < ad.length(); i++) {
-            // 전부 일치하고 광고판의 사이즈를 초과
-            if (queue.isEmpty()) {
-                return ad.toString();
+    private static int[] kmp(String input) {
+        int L = input.length();
+        int[] pi = new int[L];
+
+        // 연속된 중복 횟수 
+        int count = 0;
+
+        // 최소 문자열 길이를 찾으면 문자열 그뒤로는 count만 상승 
+        for (int i = 1; i < L; i++) {
+            
+            while (count > 0 && input.charAt(i) != input.charAt(count)) {
+                count = pi[count - 1];
             }
 
-            // 문자열 일치 확인
-            if (queue.peek().equals(ad.substring(i, i + 1))) {
-                sb.append(queue.poll());
-                continue;
+            if (input.charAt(i) == input.charAt(count)) {
+                count++;
             }
 
-            // 시작문자가 여러개 중복해서 나열되는 경우
-            if (i == dup && queue.peek().equals(ad.substring(0, 1))) {
-                String add = queue.poll();
-                ad.append(add);
-                sb.delete(0, 1);
-                sb.append(add);
-                i--;
-                continue;
-            }
-
-            break;
+            pi[i] = count;
         }
 
-        return sb.toString();
+        return pi;
     }
 }
